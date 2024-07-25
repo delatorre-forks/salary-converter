409cb56e4979ddfe41b55ab506b44f7b839324ba
Barroso Filho <barrosofilho.ti@gmail.com>
2024-07-25 01:29:25 -0300

    🚀 feat: add revalidation to currency exchange rate fetch
    Set the fetch request to revalidate every 5 minutes, ensuring more up-to-date exchange rate information for users. This improves the accuracy and reliability of the returned data.

--- src/app/api/exchange-rate/route.ts
+++ src/app/api/exchange-rate/route.ts
@@ -1,20 +1,23 @@
 export const runtime = 'edge';
 
 export async function GET() {
-    try {
-      const response = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL');
-      if (!response.ok) {
-        throw new Error('Falha ao obter a cotação');
-      }
-  
-      const data = await response.json();
-      const exchangeRate = parseFloat(data.USDBRL.ask);
-  
-      return new Response(JSON.stringify({ exchangeRate }), {
-        headers: { 'Content-Type': 'application/json' },
-      });
-    } catch (error) {
-      console.error('Erro ao buscar taxa de câmbio:', error);
-      return new Response(JSON.stringify({ error: 'Erro ao obter a taxa de câmbio' }), { status: 500 });
+  try {
+    const response = await fetch('https://economia.awesomeapi.com.br/json/last/USD-BRL', {
+      next: { revalidate: 300 } // Revalidate every 5 minutes
+    });
+
+    if (!response.ok) {
+      throw new Error('Falha ao obter a cotação');
     }
+
+    const data = await response.json();
+    const exchangeRate = parseFloat(data.USDBRL.ask);
+
+    return new Response(JSON.stringify({ exchangeRate }), {
+      headers: { 'Content-Type': 'application/json' },
+    });
+  } catch (error) {
+    console.error('Erro ao buscar taxa de câmbio:', error);
+    return new Response(JSON.stringify({ error: 'Erro ao obter a taxa de câmbio' }), { status: 500 });
   }
+}
